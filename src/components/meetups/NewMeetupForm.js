90fd02d91f35f4cb77c2be8302569a4c06cb013a
Joe Niemiec <>
2021-05-15 13:51:58 -0500

    Began adding form content

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -3,12 +3,19 @@ import classes from './NewMeetupForm.module.css';
 
 function NewMeetupForm() {
 
-    return (<Card>
-        <form className={classes.form}>
-
-        </form>
-
-    </Card>)
+    return (
+        <Card>
+            <form className={classes.form}>
+                <div className={classes.control}>
+                    <label htmlFor='title'>Meetup Title</label>
+                    <input type='text' required id='title'></input>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='image'>Meetup Image</label>
+                    <input type='url' required id='image'></input>
+                </div>
+            </form>
+        </Card>)
 
 }
 
