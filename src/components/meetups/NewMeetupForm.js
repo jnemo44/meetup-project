66f650da8c79472ae9b2bee32ff7d5e152181cfd
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-05-15 14:51:33 -0500

    Added form submission to firebase using fetch()

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,22 +1,71 @@
-import Card from '../ui/Card';
-import classes from './NewMeetupForm.module.css';
+import { useRef } from "react";
 
-function NewMeetupForm() {
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
 
-    return (
-        <Card>
-            <form className={classes.form}>
-                <div className={classes.control}>
-                    <label htmlFor='title'>Meetup Title</label>
-                    <input type='text' required id='title'></input>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor='image'>Meetup Image</label>
-                    <input type='url' required id='image'></input>
-                </div>
-            </form>
-        </Card>)
+function NewMeetupForm(props) {
+  // Creates a reference object
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    // For reading values from a form use refs. If trying to change what is displayed on the
+    // screen use state mangement
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    // Send data to 
+    props.onAddMeetup(meetupData)
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitFormHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            required
+            id="address"
+            ref={addressInputRef}
+          ></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,32 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+    const history = useHistory();
+
+
+    function addMeetupHandler(meetupPuppies) {
+        fetch(
+            'https://meetup-project-4c034-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupPuppies),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            //Good for after a form is submitted as it prevents back button from being used,
+            //routes to the home page after form submission
+            history.replace('/');
+        });
+    }
+
     return (
         <section>
             <h1>New Meetup</h1>
-            <NewMeetupForm></NewMeetupForm>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
         </section>
 
     )
