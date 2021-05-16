251d90dcf5e89b489a3a16d77e22712e15e5387f
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-05-16 14:33:07 -0500

    Finished the demo app. Added favorites tab and a favorites number badge

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,26 @@
-function FavoritesPage () {
-    return(
-        <div>
-            Favorites
-        </div>
-    )
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
+function FavoritesPage() {
+  // Get the current "state" from the favorites context
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+  // This will report a message when no favorites have been added"
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>No favorites yet. Try adding some...</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites}></MeetupList>;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
